20c9770a7495daa57d63fd1291e82ec565077988
rinarudhei <rinaldiadrian5@gmail.com>
2026-02-05 08:58:40 +0700

    feat: modify toaster message when failed to like a blog unauthorized

--- src/features/blogpost/hooks/blogpostMutation.ts
+++ src/features/blogpost/hooks/blogpostMutation.ts
@@ -14,12 +14,16 @@ export function useLikeBlogpost(
     onMutate: () => {
       setIsLiked((prev) => !prev);
     },
-    onError: () => {
-      toast.error("Failed to like blog");
+    onError: (e) => {
+      setIsLiked(false);
+      if (e.message === "Unauthorized") {
+        toast.error("Please login to like a blog");
+      } else {
+        toast.error("Failed to like blog");
+      }
     },
     onSettled: async () => {
       await Promise.all([
-        queryClient.invalidateQueries({ queryKey: ["userId-blog"] }),
         queryClient.invalidateQueries({ queryKey: ["detail-blog"] }),
         queryClient.invalidateQueries({ queryKey: ["popular-blog"] }),
         queryClient.invalidateQueries({ queryKey: ["recommended-blog"] }),
